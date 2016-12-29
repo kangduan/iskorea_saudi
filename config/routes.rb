6039849bc57ee09ac0cfdad9d855ece9e3719ab1
kangduan <duanea0606@gmail.com>
2016-12-29 03:44:48 +0000

    dfjk

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+root 'home#index'
+post 'home/write'
+
+get 'home/index'
+get 'home/about'
+get 'home/product'
 end
