7475bef29ee611ac46365cb05475ba956da7e9d7
kangduan <duanea0606@gmail.com>
2017-01-04 06:57:33 +0000

    fdk

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'home#index'
+root 'home#index_saudi'
 post 'home/write'
 
 get 'home/index_saudi'
