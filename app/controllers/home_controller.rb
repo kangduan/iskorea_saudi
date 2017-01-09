835d46a04f6eb417661e49f8a7ded707409e66de
kangduan <duanea0606@gmail.com>
2017-01-09 09:06:46 +0000

    fjdK

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     @email = params[:email]
     @content = params[:content]
     @phone = params[:phone]
-    mg_client = Mailgun::Client.new("key-ac89645311d8534bb27750e824ad715a")
+    mg_client = Mailgun::Client.new("key-dc69708e8b3f2ac481465f5d27af1693")
 
     message_params =  {
                    from: @email,
@@ -17,7 +17,7 @@ class HomeController < ApplicationController
                    text:    @content
                   }
 
-                  result = mg_client.send_message('sandbox165c26fd72294c21b95418c845d11557.mailgun.org', message_params).to_h!
+                  result = mg_client.send_message('sandbox33d60e8bc5b349c78fe359178f31bfed.mailgun.org', message_params).to_h!
 
                   message_id = result['id']
                   message = result['message']
